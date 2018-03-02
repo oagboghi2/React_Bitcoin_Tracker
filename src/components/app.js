94bee14bb83766ae39dacf6649ad4256c86e5b4a
Obo Agboghidi <obo@Obos-MacBook-Pro.local>
2018-03-01 17:33:56 -0800

    Second large commit. Got dropdowna nd 2nd API working. Will add CSS and backend tomorrow

--- src/components/app.js
+++ src/components/app.js
@@ -2,18 +2,57 @@ import React, { Component } from 'react';
 import Navbar from './navbar';
 import Logo from './bitcoin_logo'
 import Copyright from './copyright'
-import LoadButton from './bitcoin_button'
+import CoinPrice from './coin_price'
+
+const API = `https://api.coindesk.com/v1/bpi/currentprice/USD.json`
 
 
-export default class App extends Component {
+ class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data:[],
+      isLoading: false,
+      error: null
+    };
+  }
+
+  componentDidMount(){
+    this.setState({isLoading: true})
+
+      fetch(API)
+        .then(response => {
+          if(response.ok){
+            return response.json()
+          }else{
+            throw new Error('Something went wrong')
+          }
+        })
+        .then(data => this.setState({data: data.bpi.USD.rate, isLoading: false}))
+        .catch(error => this.setState({error, isLoading: false}))
+  }
+
+
   render() {
+    const {data, isLoading, error} = this.state;
+
+    if (error) {
+     return <p>{error.message}</p>;
+   }
+
+    if(isLoading){
+      return <p> Loading... </p>
+    }
+
     return (
       <div>
         <Navbar />
         <Logo />
-        <LoadButton />
+        <CoinPrice data = {this.state.data}/>
         <Copyright text="© Copyright 2018 The Awesome People" />
       </div>
     );
   }
 }
+
+export default App;
---
+++ src/components/coin_price.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+//import './button.css';
+
+
+class CoinPrice extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      price: null,
+      selectedCurrency: null
+    }
+    this.fetchPrice = this.fetchPrice.bind(this)
+  }
+
+
+  // changeCurrency(){
+  // }
+
+fetchPrice(event) {
+  console.log('value:', event.target.value);
+  let cur = event.target.value
+  fetch(`https://api.coindesk.com/v1/bpi/currentprice/${cur}.json`)
+  .then(response => {
+    if(response.ok){
+      return response.json()
+    }else{
+      return Promise.reject('Something went wrong');
+    }
+  })
+  .then(listData => {
+    this.setState({price: listData.bpi[cur].rate, selectedCurrency: cur})
+  })
+  .catch(error => this.setState({error}))
+}
+
+render(){
+
+  let priceJSX =   this.state.price ?
+    <h5>Price of 1 Bitcoin is  : {this.state.selectedCurrency}{this.state.price}</h5>
+    :
+    null
+
+  return (
+    <div>
+      <h6>Please select a currency</h6>
+      <select id = "selCurrency" onChange={this.fetchPrice}>
+           <option default>Please select a currency</option>
+           <option value = "CNY">Chinese Yuan</option>
+           <option value = "MXN">Mexico Peso</option>
+           <option value = "NGN">Nigerian Naira</option>
+           <option value = "KRW">South Korean Won</option>
+      </select>
+      {priceJSX}
+      </div>
+    )
+  }
+
+
+}
+
+export default CoinPrice
+
+
+
+/*
+practice code for rendering HTML
+
+<h5>Price of the Coin: {this.props.data}</h5>
+  <div className="dropdown">
+    <button onClick={this.openMenu} className="dropbtn"> Dropdown</button>
+    <div id="myDropdown" className="dropdown-content">
+      <a href="#"> Chinese Yuan </a>
+      <a href="#"> Mexico Peso </a>
+      <a href="#"> Nigerian Naira </a>
+      <a href="#"> South Korean Won </a>
+    </div>
+  </div>
+
+  */
